221a50fd852dd9fc9cb88d7f1a823be1cb4f7503
Bleidorb aka Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2019-09-29 13:22:45 +0200

    Add files via upload
    H002_1 JavaScript DOM

---
+++ H002_1 Javascript DOM/script/script.js
@@ -0,0 +1,12 @@
+var mijnauto = {
+	merk: "Ford",
+	type: "Mondeo",
+	aantalWielen: 4,
+	kleur: "blauw"
+}
+
+console.log("Kleur van auto is " + mijnauto.kleur);
+document.getElementById("paginatitel").innerHTML = "Dit is de paginatitel";
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+document.getElementById("aap1").src = "images/aap1.png";
+document.getElementById("aap2").src = "images/aap2.png";
