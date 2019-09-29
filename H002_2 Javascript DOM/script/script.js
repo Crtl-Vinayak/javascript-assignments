f11edc8b74bc6ff13461cde0878ba7dd7ea62180
Bleidorb aka Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2019-09-29 13:33:36 +0200

    Add files via upload
    H002_2 Javascript DOM

---
+++ H002_2 Javascript DOM/script/script.js
@@ -0,0 +1,11 @@
+document.getElementById("bl1").style.backgroundColor = "cyan";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+
+document.getElementById("bl1").style.marginTop = "0px";
+document.getElementById("bl2").style.marginTop = "20px";
+document.getElementById("bl3").style.marginTop = "50px";
+document.getElementById("bl4").style.marginTop = "0px";
+
+document.body.style.backgroundColor = "white";
