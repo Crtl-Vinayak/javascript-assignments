8819e29da85fc2ec18ab6671fe286bca99566930
Bleidorb aka Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2019-09-30 13:13:50 +0200

    Add files via upload
    H005_1 JavaScript Interactief

---
+++ H005_1 JavaScript Interactief/script/script.js
@@ -0,0 +1,116 @@
+var plaatje1 = document.getElementById("imageholder1");
+var plaatje2 = document.getElementById("imageholder2");
+var plaatje3 = document.getElementById("imageholder3");
+
+var num = 1;
+console.log(num);
+
+imageholder1.style.backgroundImage = "url('images/face1.png')";
+imageholder2.style.backgroundImage = "url('images/face2.png')";
+imageholder3.style.backgroundImage = "url('images/face3.png')";
+imageholder4.style.backgroundImage = "url('images/face4.png')";
+imageholder5.style.backgroundImage = "url('images/face5.png')";
+imageholder6.style.backgroundImage = "url('images/face6.png')";
+imageholder7.style.backgroundImage = "url('images/face7.png')";
+imageholder8.style.backgroundImage = "url('images/face8.png')";
+imageholder9.style.backgroundImage = "url('images/face9.png')";
+
+imageholder1.addEventListener("click", function() {
+	heartplace1.style.backgroundImage = "url('images/heart.png')";
+	heartplace2.style.backgroundImage = "none";
+	heartplace3.style.backgroundImage = "none";
+	heartplace4.style.backgroundImage = "none";
+	heartplace5.style.backgroundImage = "none";
+	heartplace6.style.backgroundImage = "none";
+	heartplace7.style.backgroundImage = "none";
+	heartplace8.style.backgroundImage = "none";
+	heartplace9.style.backgroundImage = "none";
+});
+imageholder2.addEventListener("click", function() {
+	heartplace2.style.backgroundImage = "url('images/heart.png')";
+	heartplace1.style.backgroundImage = "none";
+	heartplace3.style.backgroundImage = "none";
+	heartplace4.style.backgroundImage = "none";
+	heartplace5.style.backgroundImage = "none";
+	heartplace6.style.backgroundImage = "none";
+	heartplace7.style.backgroundImage = "none";
+	heartplace8.style.backgroundImage = "none";
+	heartplace9.style.backgroundImage = "none";
+});
+imageholder3.addEventListener("click", function() {
+	heartplace3.style.backgroundImage = "url('images/heart.png')";
+	heartplace1.style.backgroundImage = "none";
+	heartplace2.style.backgroundImage = "none";
+	heartplace4.style.backgroundImage = "none";
+	heartplace5.style.backgroundImage = "none";
+	heartplace6.style.backgroundImage = "none";
+	heartplace7.style.backgroundImage = "none";
+	heartplace8.style.backgroundImage = "none";
+	heartplace9.style.backgroundImage = "none";
+});
+imageholder4.addEventListener("click", function() {
+	heartplace4.style.backgroundImage = "url('images/heart.png')";
+	heartplace1.style.backgroundImage = "none";
+	heartplace2.style.backgroundImage = "none";
+	heartplace3.style.backgroundImage = "none";
+	heartplace5.style.backgroundImage = "none";
+	heartplace6.style.backgroundImage = "none";
+	heartplace7.style.backgroundImage = "none";
+	heartplace8.style.backgroundImage = "none";
+	heartplace9.style.backgroundImage = "none";
+});
+imageholder5.addEventListener("click", function() {
+	heartplace5.style.backgroundImage = "url('images/heart.png')";
+	heartplace1.style.backgroundImage = "none";
+	heartplace2.style.backgroundImage = "none";
+	heartplace3.style.backgroundImage = "none";
+	heartplace4.style.backgroundImage = "none";
+	heartplace6.style.backgroundImage = "none";
+	heartplace7.style.backgroundImage = "none";
+	heartplace8.style.backgroundImage = "none";
+	heartplace9.style.backgroundImage = "none";
+});
+imageholder6.addEventListener("click", function() {
+	heartplace6.style.backgroundImage = "url('images/heart.png')";
+	heartplace1.style.backgroundImage = "none";
+	heartplace2.style.backgroundImage = "none";
+	heartplace3.style.backgroundImage = "none";
+	heartplace4.style.backgroundImage = "none";
+	heartplace5.style.backgroundImage = "none";
+	heartplace7.style.backgroundImage = "none";
+	heartplace8.style.backgroundImage = "none";
+	heartplace9.style.backgroundImage = "none";
+});
+imageholder7.addEventListener("click", function() {
+	heartplace7.style.backgroundImage = "url('images/heart.png')";
+	heartplace1.style.backgroundImage = "none";
+	heartplace2.style.backgroundImage = "none";
+	heartplace3.style.backgroundImage = "none";
+	heartplace4.style.backgroundImage = "none";
+	heartplace5.style.backgroundImage = "none";
+	heartplace6.style.backgroundImage = "none";
+	heartplace8.style.backgroundImage = "none";
+	heartplace9.style.backgroundImage = "none";
+});
+imageholder8.addEventListener("click", function() {
+	heartplace8.style.backgroundImage = "url('images/heart.png')";
+	heartplace1.style.backgroundImage = "none";
+	heartplace2.style.backgroundImage = "none";
+	heartplace3.style.backgroundImage = "none";
+	heartplace4.style.backgroundImage = "none";
+	heartplace5.style.backgroundImage = "none";
+	heartplace6.style.backgroundImage = "none";
+	heartplace7.style.backgroundImage = "none";
+	heartplace9.style.backgroundImage = "none";
+});
+imageholder9.addEventListener("click", function() {
+	heartplace9.style.backgroundImage = "url('images/heart.png')";
+	heartplace1.style.backgroundImage = "none";
+	heartplace2.style.backgroundImage = "none";
+	heartplace3.style.backgroundImage = "none";
+	heartplace4.style.backgroundImage = "none";
+	heartplace5.style.backgroundImage = "none";
+	heartplace6.style.backgroundImage = "none";
+	heartplace7.style.backgroundImage = "none";
+	heartplace8.style.backgroundImage = "none";
+});
